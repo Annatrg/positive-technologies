bdcabccbceeb586f8e686b41f91b50b07910ab23
anna.torgova <anna.torgova@lenvendo.ru>
2023-12-10 20:36:49 +0300

    Test home page

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,6 +1,14 @@
 from selene import browser, have, be
 
 
-def test_banners(open_home_page):
+
+def test_banners():
+    browser.open('')
     browser.should(have.url('https://www.ptsecurity.com/ru-ru/'))
     browser.element('.cards').should(be.visible)
+
+
+def test_cookies():
+    browser.open('')
+    browser.element('#onetrust-accept-btn-container').click()
+    browser.element('#onetrust-accept-btn-container').should(be.not_.visible)
