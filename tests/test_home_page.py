9b48830bf953f2fc9efb3c1d41deb2e6a593fb44
anna.torgova <anna.torgova@lenvendo.ru>
2023-12-14 00:32:08 +0300

    Mini fix, assert current_url

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,4 +1,7 @@
 import allure
+from selene.support.shared import browser
+
+from allure_commons.types import Severity
 from helpers.home_page import HomePageHelper
 from helpers.universal import UniversalHelper
 
@@ -7,6 +10,8 @@ universal_helper = UniversalHelper()
 
 
 @allure.title("Проверка наличия баннеров")
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
 def test_banners():
     with allure.step('Открыть домашнюю страницу'):
         home_page.open_home_page()
@@ -17,6 +22,8 @@ def test_banners():
 
 
 @allure.title("Принять все cookies")
+@allure.tag("web")
+@allure.severity(Severity.NORMAL)
 def test_cookies():
     with allure.step('Открыть домашнюю страницу'):
         home_page.open_home_page()
@@ -24,11 +31,15 @@ def test_cookies():
         home_page.accept_cookies()
 
 
-@allure.title("Возвращение на главную страницу")
+@allure.title("Возвращение на главную страницу через логотип компании")
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
 def test_go_to_home_page():
-    with allure.step('Открыть любую страницу страницу'):
+    with allure.step('Открыть любую страницу'):
         universal_helper.open_page('/about/contacts/')
     with allure.step('Вернуться на домашнюю страницу через иконку логотипа'):
         universal_helper.return_home_page()
     with allure.step('Проверить URL страницы'):
-        universal_helper.check_url('https://www.ptsecurity.com/ru-ru/')
+        current_url = browser.driver.current_url
+        assert "https://www.ptsecurity.com/ru-ru/" in current_url, \
+            f'Текущая ссылка страницы {current_url} не соответствует ожидаемой'
--- tests/test_vacancy_page.py
+++ tests/test_vacancy_page.py
@@ -1,4 +1,6 @@
 import allure
+from allure_commons.types import Severity
+from selene.support.shared import browser
 
 from helpers.home_page import HomePageHelper
 from helpers.universal import UniversalHelper
@@ -10,33 +12,46 @@ vacancy_page = VacancyPageHelper()
 
 
 @allure.title("Переход на страницу с вакансиями")
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
 def test_go_to_vacancy_page():
     with allure.step('Открыть домашнюю страницу'):
         home_page.open_home_page()
     with allure.step('Перейти на страницу списка вакансий'):
         vacancy_page.get_to_vacancy_page()
     with allure.step('Проверить URL страницы'):
-        universal_helper.check_url('https://www.ptsecurity.com/ru-ru/about/vacancy/')
-
+        current_url = browser.driver.current_url
+        assert "https://www.ptsecurity.com/ru-ru/about/vacancy/" in current_url, \
+            f'Текущая ссылка страницы {current_url} не соответствует ожидаемой'
 
 @allure.title("Проверка фильтра по направлению вакансии")
+@allure.tag("web")
+@allure.severity(Severity.NORMAL)
 def test_profession_filters():
+    expected_vacancy = 'Информационная безопасность'
+    unexpected_vacancy = 'Тестирование'
+
     with allure.step('Открыть страницу с вакансиями'):
         universal_helper.open_page('/about/vacancy/')
     with allure.step('Выбрать фильтр по направлению деятельности'):
         vacancy_page.choose_filter('Информационная безопасность')
     with allure.step('Проверить, что в результатах есть вакансия с указанным направлением'):
-        vacancy_page.keyword_appears_in_the_results('Информационная безопасность')
+        vacancy_page.keyword_appears_in_the_results(expected_vacancy)
     with allure.step('Проверить, что в результатах нет вакансий с другим направлением'):
-        vacancy_page.keyword_missing_from_results('Тестирование')
+        vacancy_page.keyword_missing_from_results(unexpected_vacancy)
 
 
 @allure.title("Проверка открытия страницы с вакансией")
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
 def test_open_vacancy():
+    keyword_1 = 'Мы ищем технического писателя'
+    keyword_2 = 'Что мы ожидаем от кандидатов'
+
     with allure.step('Открыть страницу с вакансиями'):
         universal_helper.open_page('/about/vacancy/')
     with allure.step('Открыть страницу любой вакансии вакансии'):
         vacancy_page.open_vacancy_page('Технический писатель')
     with allure.step('Проверить, что в вакансии присутствуют ключевые слова'):
-        vacancy_page.keyword_appears_in_vacancy('Мы ищем технического писателя')
-        vacancy_page.keyword_appears_in_vacancy('Что мы ожидаем от кандидатов')
+        vacancy_page.keyword_appears_in_vacancy(keyword_1)
+        vacancy_page.keyword_appears_in_vacancy(keyword_2)
